a84b8f606d9c3017c4e86a5f2f7da24c66fa1bf8
Matt Hall <5151457+mattEhall@users.noreply.github.com>
2023-07-07 12:02:33 -0600

    Bugfix for using MoorDyn input files with coupled points for FOWTs:
    - When a FOWT has a MoorDyn style input file, and that file has
      points with "coupled" type rather than a coupled body, RAFT will
      now create a MoorPy body for the FOWT and attach the coupled
      points to it (turning them into fixed points attached to body 1).
    - FOWT objects now also save overall mass/volume properties directly
      in the FOWT object: m, v, rCG, AWP, rM.

--- raft/raft_fowt.py
+++ raft/raft_fowt.py
@@ -177,6 +177,7 @@ class FOWT():
 
         # this FOWT's own MoorPy system (may not be used)
         if design['mooring']:
+            '''
             if 'file' in design['mooring']:
                 self.ms = mp.System(design['mooring']['file'], depth = design['mooring']['water_depth'])
                 self.ms.bodyList[0].type = -1  # need to make sure it's set to a coupled type
@@ -188,8 +189,30 @@ class FOWT():
                 self.ms.bodyList[0].type = -1  # need to make sure it's set to a coupled type
                 self.ms.transform(trans=[x_ref, y_ref])  # move mooring system according to the FOWT's reference position
                 self.ms.initialize()
+            '''
+            self.ms = mp.System()
+            self.ms.parseYAML(design['mooring'])
+            
+            # ensure proper setup with one coupled Body tied to this FOWT
+            if len(self.ms.bodyList) == 0:
+                self.ms.addBody(-1, [0,0,0,0,0,0]) # create a new body if needed
+                for point in self.ms.pointList:
+                    if point.type == -1:  # attached any coupled points to the body
+                        self.ms.bodyList[0].attachPoint(point.number, point.r)
+                        point.type = 1  # now indicate point is fixed (to the body)
+                        
+            elif len(self.ms.bodyList) == 1:
+                self.ms.bodyList[0].type = -1  # ensure it's set to coupled type
+            else:
+                raise Exception("More than one body detected in FOWT mooring system.")
+                
+            # move mooring system according to the FOWT's reference position
+            self.ms.transform(trans=[x_ref, y_ref])  
+            self.ms.initialize()
+
         else:
             self.ms = None
+        
         self.F_moor0 = np.zeros(6)     # mean mooring forces in a given scenario
         self.C_moor = np.zeros([6,6])  # mooring stiffness matrix in a given scenario
 
@@ -528,7 +551,11 @@ class FOWT():
             self.body.AWP = AWP_TOT
             self.body.rM = np.array([rCB_TOT[0], rCB_TOT[1], zMeta])    # now includes x and y coordinates for center of buoyancy
         #is there any risk of additional moments due to offset CB since MoorPy assumes CB at ref point? <<<
-
+        self.m = mTOT
+        self.v = VTOT
+        self.rCG = rCG_TOT
+        self.AWP = AWP_TOT
+        self.rM = np.array([rCB_TOT[0], rCB_TOT[1], zMeta])
 
 
     def calcBEM(self, dw=0, wMax=0, wInf=10.0, dz=0, da=0, headings=[0], meshDir=os.path.join(os.getcwd(),'BEM')):

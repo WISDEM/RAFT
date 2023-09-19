8b2fedaa5d2bd05eb7c73b1cece714baa2cbf982
Stein <steinhousner@gmail.com>
2023-09-19 10:49:56 -0600

    If using l_fill = meters, stations need to also be in meters
    - Updated the VolturnUS-S_example.yaml in the examples folder to reflect this l_fill rule (and to match the VolturnUS yaml in the designs folder)
    - Wrote a better note about rA/rB in raft_member

--- raft/raft_member.py
+++ raft/raft_member.py
@@ -37,7 +37,7 @@ class Member:
         if (self.rA0[2] == 0 or self.rB0[2] == 0) and self.type != 3:
             raise ValueError("RAFT Members cannot start or end on the waterplane")
         if self.rB0[2] < self.rA0[2]:
-            raise ValueError("RAFT Members must have their rA position below the rB position")
+            raise ValueError(f"The z position of rA is {self.rA0[2]}, and the z position of rB is {self.rB0[2]}. RAFT Members must have their rA position below the rB position. Try changing your input design yaml rA/rB values.")
 
         shape      = str(mi['shape'])                                # the shape of the cross section of the member as a string (the first letter should be c or r)
 

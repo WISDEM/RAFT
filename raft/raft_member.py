ee11af5939486ac9746eef33b15b9f16605eb5da
Lucas-Carmo <lucas.carmo@nrel.gov>
2024-03-13 15:33:08 -0600

    Added member test cases + minor fix for tapered rectangular members
    - Added tests for
         - Floating pitched (0deg, 30deg, 0deg) circular cylinder (mem_srf_pitch_circ_cyl.yaml).
         - Floating pitched (0deg, 30deg, 0deg) rectangular cylinder (mem_srf_pitch_rect_cyl.yaml)
         -Floating inclined (30deg, 30deg, 30deg) circular cylinder (mem_srf_inc_rect_cyl.yaml)
         - Submerged horizontal circular cylinder (mem_subm_horz_circ_cyl.yaml) )
         - Submerged horizontal circular cylinder (mem_subm_horz_circ_cyl.yaml) )
         - Floating vertical tapered circular cylinder (mem_srf_vert_tap_circ_cyl.yaml)
         - Floating vertical tapered rectangular cylinder (mem_srf_vert_tap_rect_cyl.yaml)
    
    - Fixed a minor bug in RectangularFrustumMOI within raft_member.py. The bug only affected rectangular elements tapered along both sides (i.e. La != Lb and Wa != Wb)

--- raft/raft_member.py
+++ raft/raft_member.py
@@ -379,10 +379,10 @@ class Member:
 
                 elif La!=Lb and Wa!=Wb: # if it's a truncated pyramid for both side lengths
 
-                    x2 = (1/12)*p* ( (Lb-La)**3*H*(Wb/5 + Wa/20) + (Lb-La)**2*La*H(3*Wb/4 + Wa/4) + \
+                    x2 = (1/12)*p* ( (Lb-La)**3*H*(Wb/5 + Wa/20) + (Lb-La)**2*La*H*(3*Wb/4 + Wa/4) + \
                                      (Lb-La)*La**2*H*(Wb + Wa/2) + La**3*H*(Wb/2 + Wa/2) )
 
-                    y2 = (1/12)*p* ( (Wb-Wa)**3*H*(Lb/5 + La/20) + (Wb-Wa)**2*Wa*H(3*Lb/4 + La/4) + \
+                    y2 = (1/12)*p* ( (Wb-Wa)**3*H*(Lb/5 + La/20) + (Wb-Wa)**2*Wa*H*(3*Lb/4 + La/4) + \
                                      (Wb-Wa)*Wa**2*H*(Lb + La/2) + Wa**3*H*(Lb/2 + La/2) )
 
                     z2 = p*( Wb*Lb/5 + Wa*Lb/20 + La*Wb/20 + Wa*La*(1/30) ) * H**3

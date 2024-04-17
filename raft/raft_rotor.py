f20af4f87e3a9e01c605dc30e768477fc3a5a4bd
erickaloz <elozon@nrel.gov>
2024-04-17 14:34:38 -0600

    RAFT FLORIS edits - edits for various yaw_modes to align with changes in raft_rotor - added yaw_mode = 3 for the user to define global turbine heading using self.yaw - yaw_mode = 2 not working fully, needs to be checked - floris coupling now checks for convergence in turbine powers before completing the loop - now return turbines x, y positions, and power as arrays of data from all iterations

--- raft/raft_rotor.py
+++ raft/raft_rotor.py
@@ -682,7 +682,7 @@ class Rotor:
         
         # evaluate aero loads and derivatives with CCBlade
         loads, derivs = self.ccblade.evaluate(Uhub, Omega_rpm, pitch_deg, coefficients=True)
-        
+
         # organize and save the relevant outputs...
         self.U_case         = Uhub
         self.Omega_case     = Omega_rpm
@@ -722,7 +722,6 @@ class Rotor:
         J["T","Omega_rpm"] = np.atleast_1d(np.diag(dT["dOmega"]))
 
         self.J = J
-
         return loads, derivs
 
 
@@ -784,9 +783,13 @@ class Rotor:
             turbine_heading = np.radians(getFromDict(case, 'turbine_heading', shape=0, default=0.0))  # [rad]
             nac_yaw = turbine_heading - platform_heading
             
-        elif self.yaw_mode == 2:  # use self.yaw value
+        elif self.yaw_mode == 2:  # use self.yaw value *** turbine_platform is not defined
             nac_yaw = self.yaw
             turbine_heading = turbine_platform + nac_yaw
+            
+        elif self.yaw_mode == 3: # use self.yaw value as global heading
+            turbine_heading = self.yaw
+            nac_yaw = turbine_heading - platform_heading
             
         else:
             raise Exception('Unsupported yaw_mode value. Must be 0, 1, or 2.')
